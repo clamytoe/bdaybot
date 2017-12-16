aecad01f8ae5828bbdd57c90d2064269abafd7d4
dseptem <d.septem@gmail.com>
2017-12-16 01:52:56 -0300

    *introduced session context manager, better handling of errors *docstrings!

--- bd_db.py
+++ bd_db.py
@@ -1,7 +1,10 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine('sqlite:///bdayb.db', echo=True)
 Base = declarative_base()
@@ -33,41 +36,85 @@ class Birthday(Base):
 # create tables
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    sm = sessionmaker(bind=engine)
+    session = sm()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 def create_birthday(user, birth_date, timezone):
+    """Creates a Birthday entry with the provided user data
+
+    :param user: String - User name
+    :param birth_date: datetime.date - The birthday of the user
+    :param timezone: String - The user's timezone
+
+    :return: bool - True if succesful, else False
+    """
     new_birthday = Birthday(user, birth_date, timezone)
-    session.add(new_birthday)
-    session.commit()
-    res = session.query(Birthday).filter(Birthday.username == user).first()
-    if not res:
-        return False
-    return True
+    with session_scope() as session:
+        session.add(new_birthday)
+        session.commit()
+        res = session.query(Birthday).filter(Birthday.username == user).first()
+        if not res:
+            return False
+        return True
 
 
 def modify_birthday(user, birth_date, timezone):
-    res = session.query(Birthday).filter(Birthday.username == user).first()
-    if not res:
-        return False
-    res.birth_date = birth_date
-    res.timezone = timezone
-    session.commit()
-    return True
+    """Modifies a Birthday entry with new data
+
+    :param user: String - User name
+    :param birth_date: datetime.date - The new birthday of the user
+    :param timezone: String - The user's timezone
+
+    :return: bool - True if succesful, else False
+    """
+    with session_scope() as session:
+        res = session.query(Birthday).filter(Birthday.username == user).first()
+        if not res:
+            return False
+        res.birth_date = birth_date
+        res.timezone = timezone
+        session.commit()
+        return True
 
 
 def delete_birthday(user):
-    res = session.query(Birthday).filter(Birthday.username == user).first()
-    if not res:
-        return False
-    session.delete(res)
-    session.commit()
-    return True
+    """Deletes the Birthday entry for the provided user
+
+    :param user: String - User name
+
+    :return: bool - True if succesful, else False
+    """
+    with session_scope as session:
+        res = session.query(Birthday).filter(Birthday.username == user).first()
+        if not res:
+            return False
+        session.delete(res)
+        session.commit()
+        return True
 
 
 def retrieve_user_data(user):
-    res = session.query(Birthday).filter(Birthday.username == user).first()
-    if not res:
-        return None, None
-    return res.birth_date, res.timezone
+    """Retrieves data for the provided user
+
+    :param user: String - User name
+
+    :return: Tuple - (birth_date as datetime.date, timezone as string) or (None, None) if user is not in the database
+    """
+    with session_scope() as session:
+        res = session.query(Birthday).filter(Birthday.username == user).first()
+        if not res:
+            return None, None
+        return res.birth_date, res.timezone
