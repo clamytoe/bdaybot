9f0498b8c0e3eedccaadf71653bfd0e8ea6c9574
dseptem <d.septem@gmail.com>
2017-12-16 15:13:58 -0300

    *added reminder db object *added create and delete functions for reminders *added function to retrieve a user's existing reminders

--- bd_db.py
+++ bd_db.py
@@ -1,9 +1,9 @@
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Date, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 
 engine = create_engine('sqlite:///bdayb.db', echo=True)
@@ -19,6 +19,7 @@ class Birthday(Base):
     username = Column(String, unique=True)
     birth_date = Column(Date)
     timezone = Column(String)
+    reminders = relationship("Reminder")
 
     # ----------------------------------------------------------------------
     def __init__(self, username, birth_date, timezone):
@@ -31,6 +32,25 @@ class Birthday(Base):
         return "<Birthday (username={}, birth_date={}, timezone={})>".format(self.username,
                                                                              self.birth_date,
                                                                              self.timezone)
+
+
+class Reminder(Base):
+    """"""
+    __tablename__ = "reminders"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, ForeignKey('birthdays.username'))
+    birthday = Column(DateTime)
+
+    # ----------------------------------------------------------------------
+    def __init__(self, username, birthday):
+        """"""
+        self.username = username
+        self.birthday = birthday
+
+    def __repr__(self):
+        return "<Reminder (username={}, birthday={}>".format(self.username,
+                                                             self.birthday)
 
 
 # create tables
@@ -118,3 +138,51 @@ def retrieve_user_data(user):
         if not res:
             return None, None
         return res.birth_date, res.timezone
+
+
+def retrieve_user_reminders(user):
+    """Retrieves reminders for the provided user
+
+    :param user: String - User name
+
+    :return: List(Tuple) - (reminder id, datetime.datetime) or None if no reminders in the db
+    """
+    with session_scope() as session:
+        res = session.query(Reminder).filter(Reminder.username == user).all()
+        if not res:
+            return None
+        return [(reminder.id, reminder.birthday) for reminder in res]
+
+
+def create_reminder(user, birthday):
+    """Creates a reminder entry for a user
+
+    :param user: String - User name
+    :param birthday: datetime.datetime - The birthday of the user, timezone-adjusted for the bot
+
+    :return: bool - True if succesful, else False
+    """
+    new_reminder = Reminder(user, birthday)
+    with session_scope() as session:
+        session.add(new_reminder)
+        session.commit()
+        res = session.query(Reminder).filter(Reminder.username == user, Reminder.birthday == birthday).first()
+        if not res:
+            return False
+        return True
+
+
+def delete_reminder(r_id):
+    """Deletes the reminder entry with the specified id
+
+    :param r_id: Integer - The reminder id
+
+    :return: bool - True if succesful, else False
+    """
+    with session_scope as session:
+        res = session.query(Reminder).filter(Reminder.id == r_id).first()
+        if not res:
+            return False
+        session.delete(res)
+        session.commit()
+        return True
