888d123798b9f7eeda148c8f050cecad8eb974bd
Martin Uribe <clamytoe@users.noreply.github.com>
2018-01-02 08:22:33 -0600

    Added some tests to test_bdaybot_slack.py

--- tests/test_bdaybot_slack.py
+++ tests/test_bdaybot_slack.py
@@ -1,5 +1,31 @@
-from bdaybot import SLACK_CLIENT, SlackClient
+# -*- coding: utf-8 -*-
+import pytest
+from slackclient import SlackClient
+
+from config import BOT_ID, SLACK_BOT_TOKEN
+from bdaybot import SLACK_CLIENT
 
 
-def test_globals():
-    assert isinstance(SLACK_CLIENT, SlackClient)
+@pytest.fixture
+def slack_client():
+    return SlackClient(SLACK_BOT_TOKEN)
+
+
+def test_environment_variables():
+    assert len(BOT_ID) == 9
+    assert SLACK_BOT_TOKEN.startswith('xoxb') is True
+
+
+def test_slack_client(slack_client):
+    assert isinstance(slack_client, SlackClient)
+
+
+def test_api_call(slack_client):
+    api_call = slack_client.api_call("users.list")
+    users_dict = api_call.get('members')
+    users = [user['name'] for user in users_dict]
+
+    assert api_call.get('ok') == True
+    assert isinstance(users, list)
+    assert isinstance(users[0], dict)
+    assert 'bdaybot' in users
